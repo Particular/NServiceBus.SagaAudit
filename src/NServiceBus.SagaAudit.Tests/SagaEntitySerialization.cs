e4c24a61be95d9f56b298539e5cc46a1ad3557b3
Bob Langley <bob.langley@particular.net>
2017-11-07 07:35:36 -0800

    Add test for ServiceInsight compatibility

--- src/NServiceBus.SagaAudit.Tests/SagaEntitySerialization.cs
+++ src/NServiceBus.SagaAudit.Tests/SagaEntitySerialization.cs
@@ -1,8 +1,10 @@
 ﻿namespace NServiceBus.SagaAudit.Tests
 {
     using System;
+    using System.Linq;
     using System.Runtime.CompilerServices;
     using NUnit.Framework;
+    using ServiceInsight.Saga;
     using SimpleJson;
 
     [TestFixture]
@@ -45,6 +47,55 @@
             TestApprover.Verify(serialized);
         }
 
+        [Test]
+        public void Saga_entity_compatible_with_serviceinsight()
+        {
+            var entity = new SagaEntity
+            {
+                IntProperty = 42,
+                DateProperty = new DateTime(2017, 10, 26, 13, 3, 13, DateTimeKind.Utc),
+                NullableDateProperty = new DateTime(2013, 10, 26, 13, 3, 13, DateTimeKind.Utc),
+                GuidProperty = Guid.Empty,
+                StringProperty = "String",
+                TimeProperty = new TimeSpan(1, 2, 3, 4),
+                NullableTimeProperty = new TimeSpan(5, 6, 7, 8),
+                NestedObjectProperty = new NestedObject
+                {
+                    IntProperty = 1
+                }
+            };
+            var sagaDataJson = SimpleJson.SerializeObject(entity, new SagaEntitySerializationStrategy());
+
+            var sagaDataProperties = JsonPropertiesHelper.ProcessValues(sagaDataJson);
+
+            var jsonObj = SimpleJson.DeserializeObject(sagaDataJson) as JsonObject;
+
+            Assert.IsNotNull(jsonObj, "SimpleJson.DeserializeObject");
+
+            sagaDataProperties.ForEach(p =>
+            {
+                Assert.IsTrue(jsonObj.ContainsKey(p.Key), $"{p.Key} not found");
+
+                var expected = jsonObj[p.Key].ToString();
+                var value = p.Value;
+
+                //ServiceInsight uses default ToString() implementation, so adjust for that:
+                switch (p.Key)
+                {
+                    case "DateProperty":
+                    case "NullableDateProperty":
+                        // ReSharper disable once SpecifyACultureInStringConversionExplicitly
+                        expected = DateTime.Parse(expected).ToUniversalTime().ToString();
+                        break;
+                    case "NestedObjectProperty":
+                        value = p.Value.Replace(Environment.NewLine, string.Empty).Replace(" ", string.Empty).Replace(",NServiceBus", ", NServiceBus");
+                        break;
+                }
+
+                Assert.AreEqual(expected, value, p.Key);
+            });
+        }
+
         public class SagaEntityWithNestedObject
         {
             public NestedObject NestedObject { get; set; }
@@ -69,4 +120,24 @@
             string PrivateProperty { get; set; } = "test";
         }
     }
+}
+
+namespace ServiceInsight.Saga
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Newtonsoft.Json;
+
+    class JsonPropertiesHelper
+    {
+        static readonly IList<string> StandardKeys = new List<string> { "$type", "Id", "Originator", "OriginalMessageId" };
+
+        public static IList<KeyValuePair<string, string>> ProcessValues(string stateAfterChange) => JsonConvert.DeserializeObject<Dictionary<string, object>>(stateAfterChange)
+            .Where(m => StandardKeys.All(s => s != m.Key))
+            .Select(f => new KeyValuePair<string, string>(f.Key, f.Value?.ToString()))
+            .ToList();
+
+        public static IList<KeyValuePair<string, string>> ProcessArray(string stateAfterChange) => ProcessValues(stateAfterChange.TrimStart('[').TrimEnd(']'));
+    }
+}
 }
