4c4261d12f0a87d3bfb3cd65dd675b3c645fb663
Jan Ove Skogheim <janovesk@janovesk.com>
2020-09-10 11:12:31 +0200

    Update registrations

--- src/NServiceBus.SagaAudit.AcceptanceTests/EndpointTemplates/ConfigureExtensions.cs
+++ src/NServiceBus.SagaAudit.AcceptanceTests/EndpointTemplates/ConfigureExtensions.cs
@@ -2,7 +2,7 @@
 {
     using System.Threading.Tasks;
     using AcceptanceTesting.Support;
-    using ObjectBuilder;
+    using Microsoft.Extensions.DependencyInjection;
 
     public static class ConfigureExtensions
     {
@@ -25,12 +25,12 @@
             builder.RegisterComponents(r => { RegisterInheritanceHierarchyOfContextOnContainer(runDescriptor, r); });
         }
 
-        static void RegisterInheritanceHierarchyOfContextOnContainer(RunDescriptor runDescriptor, IConfigureComponents r)
+        static void RegisterInheritanceHierarchyOfContextOnContainer(RunDescriptor runDescriptor, IServiceCollection sc)
         {
             var type = runDescriptor.ScenarioContext.GetType();
             while (type != typeof(object))
             {
-                r.RegisterSingleton(type, runDescriptor.ScenarioContext);
+                sc.AddSingleton(type, runDescriptor.ScenarioContext);
                 type = type.BaseType;
             }
         }
