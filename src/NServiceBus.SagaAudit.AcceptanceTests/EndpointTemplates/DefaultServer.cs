5eedc10ee9d9c899e29adadb4bc3ee1b1f9f4138
Jo Palac <j_palac@hotmail.com>
2022-08-04 14:33:53 +1000

    update to json serialiser to fix build errors

--- src/NServiceBus.SagaAudit.AcceptanceTests/EndpointTemplates/DefaultServer.cs
+++ src/NServiceBus.SagaAudit.AcceptanceTests/EndpointTemplates/DefaultServer.cs
@@ -33,7 +33,7 @@
             recoverability.Delayed(delayed => delayed.NumberOfRetries(0));
             recoverability.Immediate(immediate => immediate.NumberOfRetries(0));
             configuration.SendFailedMessagesTo("error");
-            configuration.UseSerialization<NewtonsoftSerializer>();
+            configuration.UseSerialization<NewtonsoftJsonSerializer>();
 
             await configuration.DefineTransport(runDescriptor, endpointConfiguration).ConfigureAwait(false);
 
